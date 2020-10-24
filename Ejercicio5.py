e5a953fb47f164bdccf04c19cb03466c6727307d
DarwinCocom14 <60950028+DarwinEligioCocomMis@users.noreply.github.com>
2020-10-24 10:45:22 -0500

    EJERCICIOS REGEX
    EN ESTE APARTADO ALOJARÉ MIS ARCHIVOS GERENRADOS EN PYTHON UTILIZANDO EXPRESIONES REGULARES

---
+++ Ejercicio5.py
@@ -0,0 +1,25 @@
+import re
+
+regex = r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+
+test_str = ("El ex presidente de Sudáfrica, Nelson Mandela, ha fallecido a los 95 años, según ha informado el presidente de Sudáfrica, Jacob Zuma, que ha añadido que se ha ido en paz en su casa de Johannesburgo, en compañía de su familia. La muerte se produjo el jueves a las 20.50 hora local, después de una larga convalecencia por una infeccion pulmonar.Nuestra nación ha perdido a su padre. Nelson Mandela nos unió y juntos nos despedimos de él”, dijo Zuma en un mensaje televisado a toda la Mtch\n"
+	"darwin@gmail.com\n"
+	"Fuente: https://www.ejemplos.co/texto-informativo/#ixzz6bWGojPds\n"
+	"192.168.1.0\n"
+	"Mi numero de celular es el 985 134 67 87\n"
+	"Pedro Fernandez 984-324-56-87\n"
+	"Los días 25 de Abril a las 12:00 PM comienza el horario de Verano")
+
+matches = re.finditer(regex, test_str, re.MULTILINE)
+
+for matchNum, match in enumerate(matches, start=1):
+    
+    print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+    
+    for groupNum in range(0, len(match.groups())):
+        groupNum = groupNum + 1
+        
+        print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+
+# Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
+
---
+++ Ejercicio9.py
@@ -0,0 +1,24 @@
+import re
+
+regex = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)"
+
+test_str = ("El ex presidente de Sudáfrica, Nelson Mandela, ha fallecido a los 95 años, según ha informado el presidente de Sudáfrica, Jacob Zuma, que ha añadido que se ha ido en paz en su casa de Johannesburgo, en compañía de su familia. La muerte se produjo el jueves a las 20.50 hora local, después de una larga convalecencia por una infeccion pulmonar.Nuestra nación ha perdido a su padre. Nelson Mandela nos unió y juntos nos despedimos de él”, dijo Zuma en un mensaje televisado a toda la Mtch\n"
+	"darwin@gmail.com\n"
+	"Fuente: https://www.ejemplos.co/texto-informativo/#ixzz6bWGojPds\n"
+	"192.168.1.0\n"
+	"Mi numero de celular es el 985 134 67 87\n"
+	"Pedro Fernandez 984 324 56 87\n"
+	"Los días 25 de Abril a las 12:00 PM comienza el horario de Verano")
+
+matches = re.search(regex, test_str)
+
+if matches:
+    print ("Match was found at {start}-{end}: {match}".format(start = matches.start(), end = matches.end(), match = matches.group()))
+    
+    for groupNum in range(0, len(matches.groups())):
+        groupNum = groupNum + 1
+        
+        print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = matches.start(groupNum), end = matches.end(groupNum), group = matches.group(groupNum)))
+
+# Note: for Python 2.7 compatibility, use ur"" to prefix the regex and u"" to prefix the test string and substitution.
+
